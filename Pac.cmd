b1703a8f67f376b2a915fc87af6ab9753308e473
Gorstak1979 <87996620+Gorstak1979@users.noreply.github.com>
2021-07-26 19:23:54 +0200

    Update Pac.cmd

--- Pac.cmd
+++ Pac.cmd
@@ -1,1 +1,1 @@
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v AutoConfigURL /t REG_SZ /d "https://www.proxynova.com/proxy.pac" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v AutoConfigURL /t REG_SZ /d "https://github.com/Gorstak1979/Pac/blob/main/proxy.pac" /f
